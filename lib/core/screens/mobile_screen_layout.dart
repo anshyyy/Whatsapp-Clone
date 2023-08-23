cf8e6c28afe4918225388b3aefcd392363a56b88
Anshuman Sharma <anshuman9998@gmail.com>
2023-08-23 11:30:10 +0530

    online-offline

--- lib/core/screens/mobile_screen_layout.dart
+++ lib/core/screens/mobile_screen_layout.dart
@@ -1,9 +1,50 @@
+import 'package:whatsapp/auth/controller/auth_controller.dart';
 import 'package:whatsapp/exports.dart';
 import 'package:whatsapp/features/contacts/presentation/contact_list.dart';
 import 'package:whatsapp/features/select_contacts/presentation/selectContact_screen.dart';
 
-class MobileScreen extends StatelessWidget {
+class MobileScreen extends ConsumerStatefulWidget {
   const MobileScreen({super.key});
+
+  @override
+  ConsumerState<MobileScreen> createState() => _MobileScreenState();
+}
+
+class _MobileScreenState extends ConsumerState<MobileScreen>
+    with WidgetsBindingObserver {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    WidgetsBinding.instance.removeObserver(this);
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    // TODO: implement didChangeAppLifecycleState
+    super.didChangeAppLifecycleState(state);
+    switch (state) {
+      case AppLifecycleState.resumed:
+        ref.read(authControllerProvider).setUserState(true);
+        break;
+      case AppLifecycleState.inactive:
+      case AppLifecycleState.detached:
+      case AppLifecycleState.hidden:
+      case AppLifecycleState.paused:
+        ref.read(authControllerProvider).setUserState(false);
+        break;
+      default:
+        ref.read(authControllerProvider).setUserState(false);
+        break;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
--- lib/features/chat/widget/chat_list.dart
+++ lib/features/chat/widget/chat_list.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/scheduler.dart';
 import 'package:whatsapp/core/common/widgets/loader.dart';
 import 'package:whatsapp/core/model/message.dart';
 import 'package:whatsapp/exports.dart';
@@ -7,14 +8,30 @@ import 'package:whatsapp/features/chatList/widgets/sender_message.dart';
 
 import '../../../core/info.dart';
 
-class ChatList extends ConsumerWidget {
+class ChatList extends ConsumerStatefulWidget {
   final String recieverUserID;
   const ChatList({Key? key, required this.recieverUserID}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ChatList> createState() => _ChatListState();
+}
+
+class _ChatListState extends ConsumerState<ChatList> {
+  final ScrollController messageController = ScrollController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    messageController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return StreamBuilder<List<Message>>(
-        stream: ref.read(chatControllerProvider).getChatStream(recieverUserID),
+        stream: ref
+            .read(chatControllerProvider)
+            .getChatStream(widget.recieverUserID),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Loader();
@@ -23,14 +40,18 @@ class ChatList extends ConsumerWidget {
             print(snapshot.data);
             return ErrorScreen(error: snapshot.error.toString());
           }
-
+          SchedulerBinding.instance.addPersistentFrameCallback((timeStamp) {
+            messageController
+                .jumpTo(messageController.position.maxScrollExtent);
+          });
           return ListView.builder(
+            controller: messageController,
             shrinkWrap: true,
             itemCount: snapshot.data!.length,
             itemBuilder: (context, index) {
               final messageData = snapshot.data![index];
               final timeSent = messageData.timeSent;
-              print("time is here boi $timeSent");
+              //print("time is here boi $timeSent");
               if (messages[index]['isMe'] ==
                   FirebaseAuth.instance.currentUser!.uid) {
                 return MyMessageCard(
